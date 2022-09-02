2443e1ac2680b5ef7657ae56bf147a1d2edf1e7d
gaylonalfano <gaylon.alfano@gmail.com>
2022-09-02 15:17:50 +0800

    Okay, reverted back to latest spl-token version 0.3.4. Solana Cookbook has updated examples using its built-in functions createMint(), createAssociatedTokenAccount(), mintToChecked(). Still working on the initialize test, but I need test wallets both with x and y ATAs before I can start really testing.

--- tests/non-custodial-escrow.ts
+++ tests/non-custodial-escrow.ts
@@ -1,5 +1,13 @@
 import * as anchor from "@project-serum/anchor";
-import * as splToken from "@solana/spl-token";
+import {
+  createInitializeMintInstruction,
+  TOKEN_PROGRAM_ID,
+  MINT_SIZE,
+  getMinimumBalanceForRentExemptMint,
+  createMint,
+  createAssociatedTokenAccount,
+  mintToChecked,
+} from "@solana/spl-token";
 import { Program } from "@project-serum/anchor";
 import { PublicKey } from "@solana/web3.js";
 import { NonCustodialEscrow } from "../target/types/non_custodial_escrow";
@@ -16,8 +24,8 @@ describe("non-custodial-escrow", () => {
   // Need a couple wallets for buyer and seller
   const buyer = anchor.web3.Keypair.generate();
   const seller = anchor.web3.Keypair.generate();
-  let x_mint: PublicKey;
-  let y_mint: PublicKey;
+  let x_mint;
+  let y_mint;
   let seller_x_token;
   let seller_y_token;
   let buyer_x_token;
@@ -51,31 +59,85 @@ describe("non-custodial-escrow", () => {
     );
 
     // 3. Create our x and y token Mints using @solana/spl-token methods
-    x_mint = await splToken.createMint(
+    // NOTE SPL Token program has changed. Trying to use latest v0.3.4
+    // REF Cookbook: https://solanacookbook.com/references/token.html#how-to-create-a-new-token
+    x_mint = await createMint(
       provider.connection, // connection
       seller, // payer
       seller.publicKey, // mintAuthority
       seller.publicKey, // freezeAuthority?
-      6, // decimals location of the decimal place
+      6 // decimals location of the decimal place
       // (optional) keypair?
       // (optional) confirmOptions?
-      splToken.TOKEN_PROGRAM_ID // programId
+      // TOKEN_PROGRAM_ID // programId
     );
+    console.log(`x_mint: ${x_mint.toBase58()}`);
 
-    y_mint = await splToken.createMint(
+    y_mint = await createMint(
       provider.connection, // connection
       seller, // payer of tx and init fees
       seller.publicKey, // mintAuthority
       null, // freezeAuthority?
-      6, // decimals location of the decimal place
+      6 // decimals location of the decimal place
       // (optional) keypair?
       // (optional) confirmOptions?
-      splToken.TOKEN_PROGRAM_ID // programId
+      // TOKEN_PROGRAM_ID // programId
+    );
+    console.log(`y_mint: ${y_mint.toBase58()}`);
+
+    // 4. Create associated token accounts for seller's and buyer's x and y tokens
+    // 4.1 Create seller_x_token ATA
+    seller_x_token = await createAssociatedTokenAccount(
+      provider.connection, // connection
+      seller, // payer keypair,
+      x_mint, // mint pubkey
+      seller.publicKey // owner pubkey
+    );
+    // 4.2 Mint new supply and transfer to seller_x_token account
+    await mintToChecked(
+      provider.connection, //connection,
+      seller, //payer,
+      x_mint, //mint,
+      seller_x_token, //destination,
+      seller, //authority,
+      1e8, //amount,
+      8 //decimals
+      // [signer1, signer2...], // only multisig account will use
+    );
+    // 4.3 Create the seller_y_token ATA
+    seller_y_token = await createAssociatedTokenAccount(
+      provider.connection, // connection
+      seller, // payer keypair,
+      y_mint, // mint pubkey
+      seller.publicKey // owner pubkey
     );
 
-    // 4. Create associated token accounts for seller's x and y tokens
-    seller_x_token = await x_mint.createAccount(seller);
-    await x_mint.mintTo(seller_x_token, seller, [], 10_000_000_000);
+    // 4.4 Create buyer_x_token and buyer_y_token ATAs
+    buyer_x_token = await createAssociatedTokenAccount(
+      provider.connection,
+      seller,
+      x_mint,
+      buyer.publicKey
+    );
+
+    buyer_y_token = await createAssociatedTokenAccount(
+      provider.connection,
+      seller,
+      y_mint,
+      buyer.publicKey
+    );
+
+    // 4.5 Mint new y token supply and transfer to buyer_y_token account
+    await mintToChecked(
+      provider.connection,
+      seller, // payer
+      y_mint, // mint
+      buyer_y_token, // destination
+      seller, // mint authority
+      1e8, // amount
+      8 // decimals
+      // [signer1, signer2...], // only multisig account will use
+    );
   });
 
   it("Initializes escrow account with x_amount of tokens transferred from seller", async () => {
