afef8d4843086b37f9718089d315d907cfb6aa09
gaylonalfano <gaylon.alfano@gmail.com>
2022-09-11 19:09:55 +0800

    Can create ATA for sellerXToken. Have to rewrite all the functions from Tests since I'm using the wallet-adapter and Anchor workspace provider. Basically the Wallet isn't type Signer, so I need to follow Cookbook for manual functions like getAssociatedTokenAddress, createAssociatedTokenAccountInstruction, walletStore.sendTransaction as this will sign the tx using the wallet. Next, need to keep building these helpers before I can really simulate an exchange.

--- app/src/routes/escrow.svelte
+++ app/src/routes/escrow.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { clusterApiUrl, Keypair, SystemProgram, Transaction } from '@solana/web3.js';
+	import { clusterApiUrl, Keypair, PublicKey, SystemProgram, Transaction } from '@solana/web3.js';
 	import * as anchor from '@project-serum/anchor';
 	import {
 		TOKEN_PROGRAM_ID,
@@ -10,7 +10,9 @@
 		getMinimumBalanceForRentExemptMint,
 		mintToChecked,
 		getAccount,
-		getMint
+		getMint,
+		getAssociatedTokenAddress,
+		createAssociatedTokenAccountInstruction
 	} from '@solana/spl-token';
 
 	import { walletStore } from '@svelte-on-solana/wallet-adapter-core';
@@ -32,17 +34,17 @@
 	// NOTE Need to type anchor.Wallet to get 'payer' property or errors
 	const seller = ($workspaceStore.provider as anchor.AnchorProvider).wallet as anchor.Wallet;
 	const buyer = anchor.web3.Keypair.generate();
-	let x_mint: anchor.web3.PublicKey;
+	let xMint: anchor.web3.PublicKey;
 	let xMintAccount;
-	let y_mint;
+	let yMint: anchor.web3.PublicKey;
 	let yMintAccount;
-	let seller_x_token; // Associated Token Accounts
-	let seller_y_token;
-	let buyer_x_token;
-	let buyer_y_token;
+	let sellerXToken: anchor.web3.PublicKey; // Associated Token Accounts
+	let sellerYToken;
+	let buyerXToken;
+	let buyerYToken;
 	// NOTE This is just saving the Pubkey, since program creates actual account
-	const escrowed_x_token = anchor.web3.Keypair.generate();
-	console.log(`escrowed_x_token: ${escrowed_x_token.publicKey}`);
+	const escrowedXToken = anchor.web3.Keypair.generate();
+	console.log(`escrowedXToken: ${escrowedXToken.publicKey}`);
 	// NOTE This is a PDA that we'll get below
 	let escrow: anchor.web3.PublicKey;
 
@@ -63,18 +65,18 @@
 		// A: You have to build it manually! Follow Cookbook example and read Discord:
 		// REF: https://discord.com/channels/889577356681945098/889702325231427584/979766795730821200
 		// NOTE The built-in works for testing bc we use Anchor directly, but NOT in frontend!
-		// x_mint = await createMint(
+		// xMint = await createMint(
 		// 	$workspaceStore.connection,
 		// 	seller.payer, // payer (type Signer) - ERROR undefined!
 		// 	seller.publicKey, // mintAuthority
 		// 	seller.publicKey, // freezeAuthority?
 		// 	8 // decimals location of the decimal place
 		// );
-		// console.log(`x_mint: ${x_mint.toBase58()}`);
+		// console.log(`xMint: ${xMint.toBase58()}`);
 
 		const mint = Keypair.generate();
-		x_mint = mint.publicKey;
-		console.log(`x_mint: ${x_mint}`);
+		xMint = mint.publicKey;
+		console.log(`xMint: ${xMint}`);
 
 		const tx = new Transaction().add(
 			// create mint account
@@ -87,7 +89,7 @@
 			}),
 			// init mint account
 			createInitializeMintInstruction(
-				x_mint, // mint publicKey
+				xMint, // mint publicKey
 				8, // decimals
 				seller.publicKey, // mint authority
 				seller.publicKey // freeze authority
@@ -101,108 +103,165 @@
 			})}`
 		); // WORKS! Need to use walletStore instead of workspaceStore!
 
+		// FIXME
+		// Q: Why can't I get the token mint inside this function? Think it's async error...
 		// Below errors for some reason but works if I place inside separate function. Weird.
 		// ERROR: TokenAccountNotFoundError
-		// xMintAccount = await getMint($workspaceStore.connection, x_mint);
+		// xMintAccount = await getMint($workspaceStore.connection, xMint); // error
+		// NOTE Doesn't error but doesn't really have data either...
+		// xMintAccount = await $workspaceStore.connection.getAccountInfo(xMint);
 		// console.log(xMintAccount);
 	}
 
-	async function getXMint() {
-		xMintAccount = await getMint($workspaceStore.connection, x_mint);
-		console.log(xMintAccount);
+	async function handleCreateTokenY() {
+		const mint = Keypair.generate();
+		yMint = mint.publicKey;
+		console.log(`yMint: ${yMint}`);
+
+		const tx = new Transaction().add(
+			// create mint account
+			SystemProgram.createAccount({
+				fromPubkey: $walletStore.publicKey as anchor.web3.PublicKey,
+				newAccountPubkey: mint.publicKey,
+				space: MINT_SIZE,
+				lamports: await getMinimumBalanceForRentExemptMint($workspaceStore.connection),
+				programId: TOKEN_PROGRAM_ID
+			}),
+			// init mint account
+			createInitializeMintInstruction(
+				yMint, // mint publicKey
+				8, // decimals
+				seller.publicKey, // mint authority
+				seller.publicKey // freeze authority
+			)
+		);
+
+		console.log(
+			`TxHash :: ${await $walletStore.sendTransaction(tx, $workspaceStore.connection, {
+				signers: [mint]
+			})}`
+		); // WORKS! Need to use walletStore instead of workspaceStore!
 	}
 
-	/* async function handleCreateEscrowAccount() { */
-	/* 	if (escrow) { */
-	/* 		notificationStore.add({ */
-	/* 			type: 'error', */
-	/* 			message: 'Data account already exists!' */
-	/* 		}); */
-	/* 		console.log('error', 'Data account already exists!'); */
-	/* 		return; */
-	/* 	} */
+	// TODO Refactor
+	async function getXMintAccount() {
+		xMintAccount = await getMint($workspaceStore.connection, xMint);
+	}
 
-	/* 	const [escrowPDA, escrowBump] = await anchor.web3.PublicKey.findProgramAddress( */
-	/* 		[Buffer.from('escrow2'), seller.publicKey.toBuffer()], */
-	/* 		$workspaceStore.program?.programId as anchor.web3.PublicKey */
-	/* 	); */
-	/* 	escrow = escrowPDA; */
-	/* 	console.log(`escrow: ${escrow}`); */
+	async function getYMintAccount() {
+		yMintAccount = await getMint($workspaceStore.connection, yMint);
+	}
 
-	/* 	console.log( */
-	/* 		'PDA for program', */
-	/* 		$workspaceStore.program?.programId.toBase58(), */
-	/* 		'is generated :', */
-	/* 		escrowPDA.toBase58() */
-	/* 	); */
+	async function createAssociatedTokenAccounts() {
+		// NOTE Again, can't use the handy built-in methods using spl-token w/ Anchor.
+		// Instead, need to build the tx manually and send with walletStore and connection
+		// sellerXToken = await createAssociatedTokenAccount(
+		// 	$workspaceStore.connection, // connection
+		// 	seller.payer, // payer keypair,
+		// 	xMint, // mint pubkey
+		// 	seller.publicKey // owner pubkey
+		// );
+		// console.log(`sellerXToken: ${sellerXToken}`);
 
-	/* 	// Create some associated token accounts for x and y tokens for buyer and seller */
-	/* 	// Call our on-chain program's initialize() method and set escrow properties values */
-	/* 	console.log('STARTED: Initialize escrow test...'); */
-	/* 	// NOTE Results in 0.0000004 in escrowed_x_token balance */
-	/* 	const x_amount = new anchor.BN(40); */
-	/* 	const y_amount = new anchor.BN(40); // number of token seller wants in exchange for x_amount */
-	/* 	// Check whether escrow account already has data */
-	/* 	let data; */
+		sellerXToken = await getAssociatedTokenAddress(
+			xMint, // mint
+			seller.publicKey // owner
+		);
+		console.log(`sellerXToken ${sellerXToken.toBase58()}`);
 
-	/* 	// 2. Try to retreive PDA account data if it exists */
-	/* 	console.log(`Checking if escrow account ${escrow} exists...`); */
-	/* 	try { */
-	/* 		// Check whether our PDA address has an escrow account */
-	/* 		data = await $workspaceStore.program?.account.escrow.fetch(escrow); */
-	/* 		console.log('Account already exists!'); */
-	/* 	} catch (e) { */
-	/* 		console.log(`Account ${escrow} does NOT exist!`); */
-	/* 		console.log('Creating account...'); */
-	/* 		const tx = await $workspaceStore.program?.methods */
-	/* 			.initialize(x_amount, y_amount) */
-	/* 			// NOTE We only provide the PublicKeys for all the accounts. */
-	/* 			// We do NOT have to deal with isSigner, isWritable, etc. like in RAW */
-	/* 			// since we already declared that in the program Context struct. */
-	/* 			// This means Anchor will look for all that info in our struct on ENTRY! */
-	/* 			// NOTE We also don't have to pass the System Program, Token Program, and */
-	/* 			// Associated Token Program, since Anchor resolves these automatically. */
-	/* 			// NOTE Values in accounts([]) are PublicKeys! */
-	/* 			.accounts({ */
-	/* 				seller: ($workspaceStore.provider as anchor.AnchorProvider).wallet.publicKey, */
-	/* 				xMint: x_mint, */
-	/* 				yMint: y_mint, */
-	/* 				sellerXToken: seller_x_token, */
-	/* 				escrow: escrow, // created in program */
-	/* 				escrowedXToken: escrowed_x_token.publicKey, // created in program */
-	/* 				// tokenProgram: TOKEN_PROGRAM_ID, // Q: Use 2022 version? A: TOKEN_PROGRAM_ID! */
-	/* 				// rent: SYSVAR_RENT_PUBKEY, */
-	/* 				systemProgram: anchor.web3.SystemProgram.programId */
-	/* 			}) */
-	/* 			// Q: Which accounts are Signers? */
-	/* 			// A: Check IDL! Wallet and escrowed_x_token! */
-	/* 			// Q: Why is escrowed_x_token a Signer? It's just a type TokenAccount... */
-	/* 			.signers([escrowed_x_token]) */
-	/* 			.rpc({ skipPreflight: true }); */
+		const tx = new Transaction().add(
+			createAssociatedTokenAccountInstruction(
+				$walletStore.publicKey as anchor.web3.PublicKey, // payer
+				sellerXToken, // ata
+				seller.publicKey, // owner
+				xMint // mint
+			)
+		);
 
-	/* 		console.log('TxHash ::', tx); */
+		console.log(`TxHash :: ${await $walletStore.sendTransaction(tx, $workspaceStore.connection)}`); // WORKS! Need to use walletStore instead of workspaceStore!
+	}
 
-	/* 		data = await program.account.escrow.fetch(escrow); */
-	/* 	} */
+	async function mintYTokenAndTransferToBuyerYTokenAccount() {
+		// test
+	}
 
-	/* 	const escrowedXTokenAccountBalance = await provider.connection.getTokenAccountBalance( */
-	/* 		escrowed_x_token.publicKey */
-	/* 	); */
-	/* 	console.log('INITIALIZE::escrowedXTokenAccountBalance: ', escrowedXTokenAccountBalance); */
+	async function handleCreateEscrowAccount() {
+		if (escrow) {
+			notificationStore.add({
+				type: 'error',
+				message: 'Data account already exists!'
+			});
+			console.log('error', 'Data account already exists!');
+			return;
+		}
 
-	/* 	// Add to notificationStore */
-	/* 	notificationStore.add({ */
-	/* 		type: 'success', */
-	/* 		message: 'Transaction successful!', */
-	/* 		txid: tx */
-	/* 	}); */
+		const [escrowPDA, escrowBump] = await anchor.web3.PublicKey.findProgramAddress(
+			[Buffer.from('escrow2'), seller.publicKey.toBuffer()],
+			$workspaceStore.program?.programId as anchor.web3.PublicKey
+		);
+		escrow = escrowPDA;
+		console.log(`escrow: ${escrow}`);
 
-	/* 	// 3. After the transaction returns, we can fetch the state of the vote account */
-	/* 	let currentVoteAccountState = await $workspaceStore.program?.account.voteState.fetch( */
-	/* 		voteAccountPDA */
-	/* 	); */
-	/* 	voteAccount = currentVoteAccountState; */
-	/* } */
+		console.log(
+			'PDA for program',
+			$workspaceStore.program?.programId.toBase58(),
+			'is generated :',
+			escrowPDA.toBase58()
+		);
+
+		// Create some associated token accounts for x and y tokens for buyer and seller
+		// Call our on-chain program's initialize() method and set escrow properties values
+		console.log('STARTED: Initialize escrow test...');
+		// NOTE Results in 0.0000004 in escrowedXToken balance
+		const x_amount = new anchor.BN(40);
+		const y_amount = new anchor.BN(40); // number of token seller wants in exchange for x_amount
+		// Check whether escrow account already has data
+		let data;
+
+		// 2. Try to retreive PDA account data if it exists
+		console.log(`Checking if escrow account ${escrow} exists...`);
+		try {
+			// Check whether our PDA address has an escrow account
+			data = await $workspaceStore.program?.account.escrow.fetch(escrow);
+			console.log('Account already exists!');
+		} catch (e) {
+			console.log(`Account ${escrow} does NOT exist!`);
+			console.log('Creating account...');
+			const tx = await $workspaceStore.program?.methods
+				.initialize(x_amount, y_amount)
+				// NOTE We only provide the PublicKeys for all the accounts.
+				// We do NOT have to deal with isSigner, isWritable, etc. like in RAW
+				// since we already declared that in the program Context struct.
+				// This means Anchor will look for all that info in our struct on ENTRY!
+				// NOTE We also don't have to pass the System Program, Token Program, and
+				// Associated Token Program, since Anchor resolves these automatically.
+				// NOTE Values in accounts([]) are PublicKeys!
+				.accounts({
+					seller: ($workspaceStore.provider as anchor.AnchorProvider).wallet.publicKey,
+					xMint: xMint,
+					yMint: yMint,
+					sellerXToken: sellerXToken,
+					escrow: escrow, // created in program
+					escrowedXToken: escrowedXToken.publicKey, // created in program
+					// tokenProgram: TOKEN_PROGRAM_ID, // Q: Use 2022 version? A: TOKEN_PROGRAM_ID!
+					// rent: SYSVAR_RENT_PUBKEY,
+					systemProgram: anchor.web3.SystemProgram.programId
+				})
+				// Q: Which accounts are Signers?
+				// A: Check IDL! Wallet and escrowedXToken!
+				// Q: Why is escrowedXToken a Signer? It's just a type TokenAccount...
+				.signers([escrowedXToken])
+				.rpc({ skipPreflight: true });
+
+			console.log('TxHash ::', tx);
+
+			data = await $workspaceStore.program?.account.escrow.fetch(escrow);
+		}
+
+		const escrowedXTokenAccountBalance =
+			await $workspaceStore.provider?.connection.getTokenAccountBalance(escrowedXToken.publicKey);
+		console.log('INITIALIZE::escrowedXTokenAccountBalance: ', escrowedXTokenAccountBalance);
+	}
 </script>
 
 <AnchorConnectionProvider {network} {idl} />
@@ -213,9 +272,10 @@
 		>
 			Escrow
 		</h1>
-		<div class="grid grid-cols-2 gap-6 pt-2">
+		<div class="grid grid-cols-4 gap-6 pt-2">
 			<div class="form-control">
 				<button class="btn btn-accent" on:click={handleCreateTokenX}>Create Token X</button>
+				<button class="btn btn-info mt-1" on:click={getXMintAccount}>Get X Mint</button>
 				{#if xMintAccount}
 					<label class="input-group input-group-vertical pt-1">
 						<span>Mint Address</span>
@@ -270,15 +330,63 @@
 				{/if}
 			</div>
 			<div class="form-control">
-				<button class="btn btn-info" on:click={getXMint}>Get X Mint</button>
-				<label class="input-group input-group-vertical pt-1">
-					<span>Color</span>
-					<input type="text" placeholder="" class="input input-bordered" />
-				</label>
-				<label class="input-group input-group-vertical pt-1">
-					<span>Seed</span>
-					<input type="text" placeholder="" class="input input-bordered" />
-				</label>
+				<button class="btn btn-accent" on:click={handleCreateTokenY}>Create Token Y</button>
+				<button class="btn btn-info mt-1" on:click={getYMintAccount}>Get Y Mint</button>
+				{#if yMintAccount}
+					<label class="input-group input-group-vertical pt-1">
+						<span>Mint Address</span>
+						<input
+							type="text"
+							placeholder=""
+							class="input input-bordered"
+							disabled
+							bind:value={yMintAccount.address}
+						/>
+					</label>
+					<label class="input-group input-group-vertical pt-1">
+						<span>Mint Authority</span>
+						<input
+							type="text"
+							placeholder=""
+							class="input input-bordered"
+							disabled
+							value={yMintAccount.mintAuthority}
+						/>
+					</label>
+					<label class="input-group input-group-vertical pt-1">
+						<span>Freeze Authority</span>
+						<input
+							type="text"
+							placeholder=""
+							class="input input-bordered"
+							disabled
+							bind:value={yMintAccount.freezeAuthority}
+						/>
+					</label>
+					<label class="input-group input-group-vertical pt-1">
+						<span>Supply</span>
+						<input
+							type="text"
+							placeholder=""
+							class="input input-bordered"
+							disabled
+							bind:value={yMintAccount.supply}
+						/>
+					</label>
+					<label class="input-group input-group-vertical pt-1">
+						<span>Decimals</span>
+						<input
+							type="text"
+							placeholder=""
+							class="input input-bordered"
+							disabled
+							bind:value={yMintAccount.decimals}
+						/>
+					</label>
+				{/if}
+			</div>
+			<div class="form-control">
+				<button class="btn btn-accent" on:click={createAssociatedTokenAccounts}>Create ATAs</button>
 			</div>
 		</div>
 	</div>
