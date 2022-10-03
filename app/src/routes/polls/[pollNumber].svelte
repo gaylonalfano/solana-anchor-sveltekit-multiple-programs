7482eb83a925acc0b003a83eb3702238e551e65c
gaylonalfano <gaylon.alfano@gmail.com>
2022-10-03 16:40:24 +0800

    moving some code from polls to the subroute component. Need to import all the workspace and wallet, etc. Stores so I can handleCreateVote() handlers.

--- app/src/routes/polls/[pollNumber].svelte
+++ app/src/routes/polls/[pollNumber].svelte
@@ -1,4 +1,7 @@
 <script context="module">
+	// UPDATE: Originally tried to do some data fetching but not applicable
+	// in this case. Leaving for reference.
+	// TL;DR: Can access the 'page' Store directly for route params.
 	// export async function load({ params }) {
 	// 	const pollNumberFromLoad = params.pollNumber;
 
@@ -23,7 +26,7 @@
 	import { pollsStore } from '$stores/polls/polls-store';
 	import { Button } from '$lib/index';
 
-	// import type anchor from '@project-serum/anchor';
+	const network = 'http://localhost:8899';
 
 	// export let pollNumberFromLoad: string;
 
@@ -40,19 +43,171 @@
 	// 	return p.pollNumber.words[0] === parseInt($page.params.pollNumber);
 	// }) as anchor.IdlTypes<anchor.Idl>['Poll']; // NOTE MUST cast for Types!
 
-	// Q: Can I create some custom Types that align with IDL?
-	// A: Believe so! Check out PollObject type definition
-	let poll = $pollsStore.find((p) => {
-		// console.log(p.pollNumber); // BN {....}
-		// console.log(typeof p.pollNumber); // object
-		// console.log(typeof p.pollNumber.words[0]); // number
-		// console.log(typeof pollNumberFromLoad); // string
-		// return p.pollNumber.words[0] === parseInt(pollNumberFromLoad);
-		return p.pollNumber.words[0] === parseInt($page.params.pollNumber);
-	}) as PollObject; // NOTE MUST cast for Types!
+	onMount(() => {
+		// Q: Can I create some custom Types that align with IDL?
+		// A: Believe so! Check out PollObject type definition
+		let poll = $pollsStore.find((p) => {
+			return p.pollNumber.words[0] === parseInt($page.params.pollNumber);
+		}) as PollObject; // NOTE MUST cast for Types!
 
-	// Update pollStore
-	pollStore.set(poll);
+		// Update pollStore
+		pollStore.set(poll);
+	});
+
+	// // Q: Refactor with params?
+	// // e.g., pollPubkey, voteOption?
+	// // TODO Gonna have to grab the Profile and selected Poll once we start
+	// // adding multiple polls, etc. May need to use route params or something
+	// // U: Yep, added a subroute [pollNumber] and using 'page' Store for params
+	// // TODO Could consider making a Poll component
+	// async function handleCreateVoteForOptionA() {
+	// 	// Need to access current poll.voteCount
+	// 	// Q: Need profile and/or customProgram? Or, just pass as accounts?
+	// 	// A: Eventually will need to increment/update values, but not yet!
+	// 	let currentPoll = await $workspaceStore.program?.account.poll.fetch(pollPda);
+	// 	poll = currentPoll as anchor.IdlTypes<anchor.Idl>['Poll'];
+	// 	pollStore.set(poll);
+	// 	const voteCount: string = ($pollStore.voteCount.toNumber() + 1).toString();
+	// 	console.log('voteCount: ', voteCount);
+
+	// 	const [pda, bump] = await PublicKey.findProgramAddress(
+	// 		[
+	// 			anchor.utils.bytes.utf8.encode(VOTE_SEED_PREFIX),
+	// 			pollPda.toBuffer(), // Q: Can get I PDA from pollStore?
+	// 			($walletStore.publicKey as anchor.web3.PublicKey).toBuffer()
+	// 		],
+	// 		$workspaceStore.program?.programId as anchor.web3.PublicKey
+	// 	);
+	// 	// Update global state for vote?
+
+	// 	console.log(
+	// 		'PDA for program',
+	// 		$workspaceStore.program?.programId.toBase58(),
+	// 		'is generated :',
+	// 		pda.toBase58()
+	// 	);
+
+	// 	// Following this example to call the methods:
+	// 	// https://book.anchor-lang.com/anchor_in_depth/milestone_project_tic-tac-toe.html?highlight=test#testing-the-setup-instruction
+	// 	const tx = await $workspaceStore.program?.methods
+	// 		.createVote({ a: {} })
+	// 		.accounts({
+	// 			vote: pda,
+	// 			poll: pollPda,
+	// 			profile: profilePda,
+	// 			customProgram: customProgramPda,
+	// 			authority: $walletStore.publicKey as anchor.web3.PublicKey,
+	// 			systemProgram: anchor.web3.SystemProgram.programId
+	// 		})
+	// 		// .signers([testUser2]) // AnchorWallet
+	// 		.rpc();
+	// 	console.log('TxHash ::', tx);
+
+	// 	// Fetch data after tx confirms & update global state
+	// 	const currentVote = await $workspaceStore.program?.account.vote.fetch(pda);
+	// 	vote = currentVote as anchor.IdlTypes<anchor.Idl>['Vote'];
+	// 	// Q: Need a voteStore? Eh....
+
+	// 	currentPoll = await $workspaceStore.program?.account.poll.fetch(pollPda);
+	// 	poll = currentPoll as anchor.IdlTypes<anchor.Idl>['Poll'];
+	// 	pollStore.set(poll);
+
+	// 	const currentProfile = await $workspaceStore.program?.account.profile.fetch(profilePda);
+	// 	profile = currentProfile as anchor.IdlTypes<anchor.Idl>['Profile'];
+	// 	profileStore.set(profile);
+
+	// 	const currentCustomProgram = await $workspaceStore.program?.account.customProgram.fetch(
+	// 		customProgramPda
+	// 	);
+	// 	customProgram = currentCustomProgram as anchor.IdlTypes<anchor.Idl>['CustomProgram'];
+	// 	// Q: update() or set() Store?
+	// 	customProgramStore.set(customProgram);
+	// }
+
+	// async function handleCreateVoteForOptionB() {
+	// 	// Need to access current poll.voteCount
+	// 	// Q: Need profile and/or customProgram? Or, just pass as accounts?
+	// 	// A: Eventually will need to increment/update values, but not yet!
+	// 	let currentPoll = await $workspaceStore.program?.account.poll.fetch(pollPda);
+	// 	poll = currentPoll as anchor.IdlTypes<anchor.Idl>['Poll'];
+	// 	pollStore.set(poll);
+	// 	const voteCount: string = ($pollStore.voteCount.toNumber() + 1).toString();
+	// 	console.log('voteCount: ', voteCount);
+
+	// 	const [pda, bump] = await PublicKey.findProgramAddress(
+	// 		[
+	// 			anchor.utils.bytes.utf8.encode(VOTE_SEED_PREFIX),
+	// 			pollPda.toBuffer(), // Q: Can get I PDA from pollStore?
+	// 			($walletStore.publicKey as anchor.web3.PublicKey).toBuffer()
+	// 		],
+	// 		$workspaceStore.program?.programId as anchor.web3.PublicKey
+	// 	);
+	// 	// Update global state for vote?
+
+	// 	console.log(
+	// 		'PDA for program',
+	// 		$workspaceStore.program?.programId.toBase58(),
+	// 		'is generated :',
+	// 		pda.toBase58()
+	// 	);
+
+	// 	// Following this example to call the methods:
+	// 	// https://book.anchor-lang.com/anchor_in_depth/milestone_project_tic-tac-toe.html?highlight=test#testing-the-setup-instruction
+	// 	const tx = await $workspaceStore.program?.methods
+	// 		.createVote({ b: {} })
+	// 		.accounts({
+	// 			vote: pda,
+	// 			poll: pollPda,
+	// 			profile: profilePda,
+	// 			customProgram: customProgramPda,
+	// 			authority: $walletStore.publicKey as anchor.web3.PublicKey,
+	// 			systemProgram: anchor.web3.SystemProgram.programId
+	// 		})
+	// 		// .signers([testUser2]) // AnchorWallet
+	// 		.rpc();
+	// 	console.log('TxHash ::', tx);
+
+	// 	// Fetch data after tx confirms & update global state
+	// 	const currentVote = await $workspaceStore.program?.account.vote.fetch(pda);
+	// 	vote = currentVote as anchor.IdlTypes<anchor.Idl>['Vote'];
+	// 	// Q: Need a voteStore? Don't think so since I have pollStore...
+
+	// 	currentPoll = await $workspaceStore.program?.account.poll.fetch(pollPda);
+	// 	poll = currentPoll as anchor.IdlTypes<anchor.Idl>['Poll'];
+	// 	pollStore.set(poll);
+
+	// 	const currentProfile = await $workspaceStore.program?.account.profile.fetch(profilePda);
+	// 	profile = currentProfile as anchor.IdlTypes<anchor.Idl>['Profile'];
+	// 	profileStore.set(profile);
+
+	// 	const currentCustomProgram = await $workspaceStore.program?.account.customProgram.fetch(
+	// 		customProgramPda
+	// 	);
+	// 	customProgram = currentCustomProgram as anchor.IdlTypes<anchor.Idl>['CustomProgram'];
+	// 	// Q: update() or set() Store?
+	// 	customProgramStore.set(customProgram);
+	// }
+
+	// // Q: Can I use this to update/fetch my Stores?
+	// async function derivePda(seeds: Buffer[]) {
+	// 	// NOTE This is key! We can derive PDA WITHOUT hitting our program!
+	// 	// Then we can use this PDA address in our functions as a check to see
+	// 	// whether there is a ledger account at this PDA address.
+	// 	// Then, MOST IMPORTANTLY, we can fetch the account's data from the CLIENT
+	// 	// and use its data.
+	// 	// NOTE pubkey is actually provider.wallet.publicKey
+	// 	let [pda, _] = await anchor.web3.PublicKey.findProgramAddress(
+	// 		seeds,
+	// 		$workspaceStore.program?.programId as anchor.web3.PublicKey
+	// 	);
+
+	// 	return pda;
+	// }
+
+	$: {
+		console.log('$pollsStore: ', $pollsStore);
+		console.log('$pollStore: ', $pollStore);
+	}
 
 	// <AnchorConnectionProvider {idl} {network} />
 	// <div class="stats shadow">
@@ -71,14 +226,6 @@
 	// 		>
 	// 	</div>
 	// </div>
-
-	$: {
-		console.log('FROM [pollNumber].svelte ======');
-		console.log('$pollsStore: ', $pollsStore);
-		console.log('$pollStore: ', $pollStore);
-		console.log('poll: ', poll);
-	}
 </script>
 
-<h3>poll:</h3>
-<pre>{JSON.stringify(poll, null, 2)}</pre>
+<pre>{JSON.stringify($pollStore, null, 2)}</pre>
