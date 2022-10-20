4569a535510e0e095e5eed4c1f4e6043df4efb59
gaylonalfano <gaylon.alfano@gmail.com>
2022-10-20 09:12:29 +0800

    Okay, [pda] route is doing better. On refresh and or wallet swap, it reactively gets the pollStore data. tick().then() combined with some conditional reactive statements seem to do the trick. tick() fires a lot though so maybe some areas to improve. Next, need to consider adding a getPolls helper to the pollsStore. Also need to start saving Votes per Poll, so when a user goes to a poll, the UI knows whether they've voted already.

--- app/src/routes/polls/[pda].svelte
+++ app/src/routes/polls/[pda].svelte
@@ -49,13 +49,24 @@
 	// 	// return p.pollNumber.words[0] === parseInt(pollNumberFromLoad);
 	// 	return p.pollNumber.words[0] === parseInt($page.params.pollNumber);
 	// }) as anchor.IdlTypes<anchor.Idl>['Poll']; // NOTE MUST cast for Types!
-	$: {
-    console.log('$workspaceStore: ', $workspaceStore);
-    console.log('$profileStore: ', $profileStore);
-		console.log('$pollsStore: ', $pollsStore);
-		console.log('$pollStore: ', $pollStore);
-	}
+	// $: {
+ //    console.log('$workspaceStore: ', $workspaceStore);
+ //    console.log('$profileStore: ', $profileStore);
+	// 	console.log('$pollsStore: ', $pollsStore);
+	// 	console.log('$pollStore: ', $pollStore);
+	// }
 
+  // Q: tick() needed if I add the conditional check?
+  // U: Seems needed as it doesn't error. However, tick() fires a lot
+  // and may not be most effective. Could consider cleaning up or splitting
+  // into multiple reactives per condition.
+  // NOTE On reload, this triggers 4 times for pollStore.getPollAccount
+  // NOTE async/await has no improvement/effect!
+  // Q: What about separating into separate statements?
+  // A: NOPE! Still triggers pollStore.getPollAccount() 4 times.
+  // Q: Can I use this outside of tick()?
+  // U: Kinda works but getting uncaught error with gPA inside pollStore.getPollAccount()
+  // but the pollStore doesn't get updated...
   $: tick().then(() => {
     if($walletStore.connected && !$walletStore.connecting && !$walletStore.disconnecting) {
       // Attempt to get stores (if exists)
@@ -84,68 +95,13 @@
         $workspaceStore.connection
       )
     }
-    // TODO Consider the case when pollsStore is available (but not pollStore)
-    // TODO Add a getPolls() helper to pollsStore perhaps
   }) // WORKS on refresh! async/await not needed it seems...
 
-	// onMount(() => {
- //    console.log("ONMOUNT")
-	// 	// Q: Can I create some custom Types that align with IDL?
-	// 	// A: Believe so! Check out PollObject type definition
-	// 	// let poll = $pollsStore.find((p) => {
-	// 	// 	return p.pollNumber.words[0] === parseInt($page.params.pollNumber);
-	// 	// }) as PollObject; // NOTE MUST cast for Types!
 
 
- //      // // Set/update the single pollStore
- //    //   try {
- //    //     console.log($pollsStore);
- //    //     if ($pollsStore.length > 0) {
- //    //       console.log($pollsStore);
- //    //       let { poll, pda } = $pollsStore.find((p: PollStore) => {
- //    //         return p.pda?.toBase58() === $page.params.pda;
- //    //       }) as PollObject; // NOTE MUST cast for Types!
+  // TODO Consider the case when pollsStore is available (but not pollStore)
+  // TODO Add a getPolls() helper to pollsStore perhaps
 
- //    //       // Update pollStore
- //    //       pollStore.set({ poll, pda });
- //    //     } else {
- //    //       // Fresh fetch?
- //    //       pollStore.getPollAccount(new PublicKey($page.params.pda));
- //    //     }
- //    //   } catch (e) {
- //    //     console.log(e);
- //    //   } 
-
- //    // U: Added a another helper directly on store
- //    // TODO Need to prevent pollsStore constantly adding duplicates
- //    if (!$pollStore) {
- //      console.log('$pollStore NOT found. Checking $pollsStore...');
- //      if (!$pollsStore) {
- //        console.log('$pollsStore NOT found. Getting Polls data...')
- //        // Need fresh fetch
- //        pollStore.getPollAccountFromRequest(
- //          $workspaceStore.program?.programId as anchor.web3.PublicKey,
- //          new PublicKey($page.params.pda),
- //          $workspaceStore.connection
- //        )
- //      } else {
- //        // pollsStore present but no pollStore. Attempt to find.
- //        const currentPollStore = $pollsStore.find(p => p.pda?.toBase58() === $page.params.pda) as PollStore;
- //        console.log('currentPollStore: ', currentPollStore);
- //        // Check that it was found
- //        if (currentPollStore) {
- //          pollStore.set({ poll: currentPollStore.poll, pda: currentPollStore.pda });
- //        } else {
- //          console.log('currentPollStore not found! Invoking pollStore.getPollAccount...')
- //          pollStore.getPollAccount(new PublicKey($page.params.pda));
- //        }
- //      }
- //    }
-
- //    // pollStore present
- //    console.log('$pollStore from ONMOUNT: ', $pollStore);
- //   
-	// });
 
   beforeUpdate(() => console.log('Component BEFORE UPDATE.'));
 	afterUpdate(() => console.log('Component AFTER UPDATE.\n =================='));
